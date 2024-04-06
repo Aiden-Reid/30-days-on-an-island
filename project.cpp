7eb2ce21bb9b31bba0d9a1e15bb05cb081592b12
Aiden-Reid <166103452+Aiden-Reid@users.noreply.github.com>
2024-04-05 16:07:42 -0800

    Update project.cpp
    added more items structures (weather, illnesses) and added more functions

--- project.cpp
+++ project.cpp
@@ -1,3 +1,4 @@
+#include <algorithm> //vector functions
 #include <cstdlib> //rand()
 #include <cstring> //string methods
 #include <ctime> //time(NULL)
@@ -7,7 +8,192 @@
 #include <windows.h> //Sleep()
 using namespace std;
 
-void startingPrompt(){ //if you add supplies make sure you add to switch statement
+/*
+Structures
+*/
+struct items{
+	/*
+	Used to store how much of a fruit/animal/event affects stats
+	*/
+	string name;
+	int hunger=0; //each stat default is 0
+	int thirst=0;
+	int energy=0;
+	bool isHostile=0;
+};
+
+//supplies and non-food island items
+
+struct items water {"water", 0, 30, 0};
+
+struct items firstAidKit {"first aid kit", 0, 0, 50};
+
+struct items cannedBeans {"canned beans", 0, 20, 0};
+
+struct items lighter {"lighter", 0, 0, 10};
+
+struct items rope {"rope", 0, 0, 20};
+
+struct items radio {"radio", 0, 0, 0};
+
+struct items campfire {"campfire", 0, 0, 15};
+
+//illnesses and weather
+struct items foodPoisoning {"food poisoning", -30, -30, -30, 1};
+
+struct items rationing {"rationing", -20, -20, 5, 1};
+
+struct items heatWave {"heat wave", -20, -40, -40, 1};
+
+//fruits and animals
+struct items coconut {"coconut", 5, 0, -5};
+
+struct items berries {"berries", 2, 0, -2}; //chance to be poisonous?
+
+struct items gatheredWater {"water", 0, rand()%(30-10)+10, -10};//gain random amount of thirst from 10 to 30
+
+struct items fish {"fish", 20, 0 -20};
+
+struct items chicken {"chicken", 15, 0, -15};
+
+struct items boar {"boar", 40, 0, -30, 1};
+
+struct items sleep {"sleep", -5, -10, 40};
+//end of items
+
+/*
+Basic Functions
+*/
+void startingPrompt();
+
+void choosingSupplies(char, int &, const int , vector<string> &, int &);
+
+void viewStats(int, int, int, vector<string>);
+
+void stringToLower(string &);
+
+bool inStringVector(vector<string>, string);
+
+bool inCharVector(vector<char>, char);
+
+bool inIntVector(vector<int>, int);
+
+void adjustStat(int &);
+
+/*
+Scenarios
+Seperate by categories:
+collecting supplies (shelter, fire, food, water, random washup), weather, illness
+*/
+
+//Collecting Supplies
+void shelter(bool &, int &, vector<string> &);
+
+//Weather
+void weather(items, int &, int &, int &);
+
+
+int main(){
+	/*
+	variables
+	*/
+	const int MAX_SUPPLY_COUNT=3;
+	bool hasShelter=0;
+	int hunger=0, thirst=0, energy=0, supplyCount=0, dayCount=0, rescueEarlyChance, radioBonus=0;
+	int scenario;
+	vector<string> inventory;
+	char choice;
+	vector<int> occuredScenarios; //if a scenario has occured, its corresponding integer is added
+	
+	
+	startingPrompt();
+	choosingSupplies(choice, supplyCount, MAX_SUPPLY_COUNT, inventory, radioBonus);
+	cout<<endl;
+	
+	//generating random starting stats
+	srand(time(NULL)); 
+	hunger=rand()%(100-76)+75;
+	thirst=rand()%(100-76)+75;
+	energy=rand()%(100-76)+75;
+	
+	Sleep(1000);
+	cout<<"You've arrived on the island with your supplies. "<<flush;
+	Sleep(1000);
+	cout<<"Help will arrive in 30 days."<<endl;
+	Sleep(1000);
+	cout<<"Your goal is to survive until help arrives. "<<flush;
+	Sleep(2000);
+	cout<<"Good Luck.\n"<<endl;
+	Sleep(1000);
+	viewStats(hunger, thirst, energy, inventory);
+	
+	while((hunger>0&&thirst>0&&energy>0)&&(dayCount<30)){ //loop ends when any of the stats go below 0 or survived 30 days
+		dayCount++;
+		cout<<"Day: "<<dayCount<<endl;
+		Sleep(1000);
+		
+		if(dayCount>=15){ //chance to get rescued early
+			rescueEarlyChance=1+rand()%(100-radioBonus); //if chose radio 10%, else 5%
+			if(rescueEarlyChance<=5){
+				rescueEarlyChance=0;
+				break;
+			}
+		}
+		
+		
+		if(!hasShelter){ //if the user does not have shelter built
+			shelter(hasShelter, energy, inventory);
+		}
+		
+		// viewStats(hunger, thirst, energy, inventory);
+		cout<<"\n------------------------------------------------------------------------------"<<endl;
+	}//end of while loop
+	
+	
+	
+	/*
+	Final Messages
+	*/
+	if(dayCount==30){
+		cout<<"You did it!"<<endl;
+		Sleep(4000);
+		cout<<"You were rescued and they made a Hollywood movie about you starring Tom Hanks.\n"<<flush;
+		Sleep(4000);
+		cout<<"Good job!"<<endl;
+	}
+	else if(rescueEarlyChance==0){
+		cout<<"You were rescued early on day "<<dayCount<<"!"<<endl;
+		Sleep(4000);
+		cout<<"You were brought to home safely and you were later interviewed by international"<<endl;
+		cout<<"news networks and you published a book about your story."<<endl;
+		Sleep(5000);
+		cout<<"Good job!"<<endl;
+	}
+	else{
+		if(hunger<=0){
+			cout<<"You got too hungry and died on the island on day "<<dayCount<<endl;
+			Sleep(3000);
+			cout<<"Better luck next time!\n";
+		}
+		else if(thirst<=0){
+			cout<<"You got too thirsty and died on the island on day "<<dayCount<<endl;
+			Sleep(3000);
+			cout<<"Better luck next time!\n";
+		}
+		else{
+			cout<<"You ran out of energy and died on the island on day "<<dayCount<<endl;
+			Sleep(3000);
+			cout<<"Better luck next time!\n";
+		}
+	}
+	
+	return 0;
+}
+
+/*
+Basic Functions
+*/
+void startingPrompt(){ //if you add supplies make sure you add choosingSupplies
 	/*
 	Prints the prompt when you first start the game.
 	*/
@@ -32,6 +218,7 @@ void startingPrompt(){ //if you add supplies make sure you add to switch stateme
 
 void choosingSupplies(char choice, int &supplyCount, const int MAX_SUPPLY_COUNT, vector<string> &inventory, int &radioBonus){
 	while(supplyCount<MAX_SUPPLY_COUNT){
+		cout<<">>";
 		cin>>choice;
 		switch(choice){
 			case 'a': inventory.push_back("water"); cout<<"You chose water.\n"; break;
@@ -90,21 +277,6 @@ void viewStats(int hunger, int thirst, int energy, vector<string> inventory){
 	cout<<".\n"<<endl;
 }
 
-void wait(char c){
-	/*
-	Used to give player time to read the text. The player enters anything when they are ready
-	to continue.
-	Parameters: A character variable
-	Output: Nothing
-	*/
-	cout<<"Enter anything to continue: ";
-	if(!(cin>>c)){
-		cin.clear();
-		cin.ignore('\n');
-	}
-	cout<<endl;
-}
-
 void stringToLower(string &s){
 	/*
 	Converts string to all lowercase letters
@@ -158,9 +330,6 @@ bool inIntVector(vector<int> v, int x){
 	return 0;
 }
 
-/*
-Scenarios
-*/
 void adjustStat(int &stat_){
 	/*
 	If a stat goes above 100, it is set to 100. This is because each stat is out of 100.
@@ -172,19 +341,53 @@ void adjustStat(int &stat_){
 	}
 }
 
-void shelter(bool &hasShelter, int &energy){
+/*
+Scenarios
+Seperate by categories:
+collecting supplies (shelter, fire, food, water, random washup), weather, illness
+*/
+
+//Collecting Supplies
+void shelter(bool &hasShelter, int &energy, vector<string> &inventory){
 	/*
 	Asks user if they want to build a shelter.
-	Parameters: pass boolean hasShelter and int energy by reference
-	Outputs: none; only changes value of hasShelter and energy
+	Parameters: pass boolean hasShelter, int energy, and string vectory inventory by reference
+	Outputs: none; only changes value of hasShelter, energy, and inventory if user has a rope and
+			 chooses to use it
 	*/
 	string choice="";
+	
 	cout<<"Would you like to build a shelter? This will cost 40 energy."<<endl;
 	while(choice!="yes"&&choice!="no"){
+		cout<<">>";
 		cin>>choice;
 		stringToLower(choice);
 		if(choice=="yes"){
-			hasShelter=1;
+			if(inStringVector(inventory, "rope")){ //if user has rope in their inventory
+				choice="";
+				cout<<"Would you like to use your rope to build the shelter?"<<endl;
+				Sleep(1000);
+				cout<<"This will take 20 less energy than building the shelter without a rope."<<endl;
+				while(choice!="yes"&&choice!="no"){
+					cout<<">>";
+					cin>>choice;
+					stringToLower(choice);
+					if(choice=="yes"){
+						energy+=20;
+						inventory.erase(remove(inventory.begin(), inventory.end(), "rope"), inventory.end());
+						//add 20 energy so when the energy is subtracted 40, the net change is -20
+						//also, remove "rope" from inventory
+						cout<<"You have used your rope."<<endl;
+					}
+					else if(choice=="no"){
+						cout<<"You have decided to build a shelter without using your rope."<<endl;
+					}
+					else{
+						cout<<"Not a valid answer. Please type 'yes' or 'no'."<<endl;
+					}
+				}
+			}
+			Sleep(500);
 			energy-=40;
 			cout<<"You have built a shelter to protect yourself."<<endl;
 			hasShelter=1;
@@ -197,179 +400,29 @@ void shelter(bool &hasShelter, int &energy){
 			cout<<"Not a valid answer. Please type 'yes' or 'no'."<<endl;
 		}
 	}
+	Sleep(2000);
 }
 
-/*
-Structures
-*/
-struct items{
+//Weather
+void weather(items weather, int &hunger, int &thirst, int &energy){
 	/*
-	Used to store how much of a fruit/animal/event affects stats
+	Prints out that there has been a disaster and what disaster it is
+	Parameters: Pass the disaster by referece, and pass hunger, thirst, 
+	and energy by reference
+	Outputs: Changes hunger, thirst, and energy accordingly.
 	*/
-	string name;
-	int hunger=0; //each stat default is 0
-	int thirst=0;
-	int energy=0;
-};
-
-int main(){
-	//supplies and non-food island items
-	items water;
-	water.name="water";
-	water.thirst=30;
-	
-	items firstAidKit;
-	firstAidKit.name="first aid kit";
-	firstAidKit.energy=50;
-	
-	items cannedBeans;
-	cannedBeans.name="canned beans";
-	cannedBeans.hunger=20;
-	
-	items campfire;
-	campfire.name="campfire";
-	campfire.energy=15;
-	
-	
-	//illnesses and weather
-	items foodPoisoning;
-	foodPoisoning.name="food poisoning";
-	foodPoisoning.hunger=-30;
-	foodPoisoning.thirst=-30;
-	foodPoisoning.energy=-30;
-	
-	items rationing;
-	rationing.name="rationing";
-	rationing.hunger=-20;
-	rationing.thirst=-20;
-	rationing.energy=5;
-	
-	//fruits and animals
-	items coconut;
-	coconut.name="coconut";
-	coconut.hunger=5; //gains hunger when obtained
-	coconut.energy=-5; //loses energy to get
-	
-	items berries; //chance to be poisonous?
-	berries.name="berries";
-	berries.hunger=2;
-	berries.energy=-2;
-	
-	items gatheredWater;
-	gatheredWater.name="water";
-	gatheredWater.thirst=rand()%(30-10)+10; //gain random amount of thirst from 10 to 30
-	gatheredWater.energy=10;
-	
-	items fish;
-	fish.name="fish";
-	fish.hunger=20;
-	fish.energy=-20;
-	
-	items chicken;
-	chicken.name="chicken";
-	chicken.hunger=15;
-	chicken.energy=-15;
-	
-	items boar;
-	boar.name="boar";
-	boar.hunger=40;
-	boar.energy=-30;
-	
-	items sleep;
-	sleep.name="sleep";
-	sleep.hunger=-5;
-	sleep.thirst=-10;
-	sleep.energy=40;
-	//end of items
-	
-	/*
-	variables
-	*/
-	const int MAX_SUPPLY_COUNT=3;
-	bool hasShelter=0;
-	int hunger=0, thirst=0, energy=0, supplyCount=0, dayCount=0, rescueEarlyChance, radioBonus=0;
-	int scenario;
-	vector<string> inventory;
-	char choice;
-	vector<int> occuredScenarios; //if a scenario has occured, its corresponding integer is added
-	
-	
-	startingPrompt();
-	choosingSupplies(choice, supplyCount, MAX_SUPPLY_COUNT, inventory, radioBonus);
-	cout<<endl;
-	
-	//generating random starting stats
-	srand(time(NULL)); 
-	hunger=rand()%(100-76)+75;
-	thirst=rand()%(100-76)+75;
-	energy=rand()%(100-76)+75;
-	
-	Sleep(1000);
-	cout<<"You've arrived on the island with your supplies. "<<flush;
-	Sleep(1000);
-	cout<<"Help will arrive in 30 days."<<endl;
-	Sleep(1000);
-	cout<<"Your goal is to survive until help arrives. "<<flush;
-	Sleep(2000);
-	cout<<"Good Luck.\n"<<endl;
-	Sleep(1000);
-	viewStats(hunger, thirst, energy, inventory);
-	
-	while((hunger>0&&thirst>0&&energy>0)&&(dayCount<30)){ //loop ends when any of the stats go below 0 or survived 30 days
-		dayCount++;
-		cout<<"Day: "<<dayCount<<endl;
-		Sleep(1000);
-		
-		if(dayCount>=15){ //chance to get rescued early
-			rescueEarlyChance=1+rand()%(100-radioBonus); //if chose radio 10%, else 5%
-			if(rescueEarlyChance<=5){
-				rescueEarlyChance=0;
-				break;
-			}
-		}
-		
-		
-		if(!hasShelter){ //if the user does not have shelter built
-			shelter(hasShelter, energy);
-		}
-		
-		// viewStats(hunger, thirst, energy, inventory);
-		cout<<"\n------------------------------------------------------------------------------"<<endl;
-	}//end of while loop
-	
-	
-	if(dayCount==30){
-		cout<<"You did it!"<<endl;
-		Sleep(4000);
-		cout<<"You were rescued and they made a Hollywood movie about you starring Tom Hanks.\n"<<flush;
-		Sleep(4000);
-		cout<<"Good job!"<<endl;
-	}
-	else if(rescueEarlyChance==0){
-		cout<<"You were rescued early on day "<<dayCount<<"!"<<endl;
-		Sleep(4000);
-		cout<<"You were brought to home safely and you were later interviewed by international"<<endl;
-		cout<<"news networks and you published a book about your story."<<endl;
-		Sleep(5000);
-		cout<<"Good job!"<<endl;
+	cout<<"There has been a "<<weather.name<<endl;
+	if(weather.isHostile){
+		cout<<"You have lost: "<<endl;
 	}
 	else{
-		if(hunger<=0){
-			cout<<"You got too hungry and died on the island on day "<<dayCount<<endl;
-			Sleep(3000);
-			cout<<"Better luck next time!\n";
-		}
-		else if(thirst<=0){
-			cout<<"You got too thirsty and died on the island on day "<<dayCount<<endl;
-			Sleep(3000);
-			cout<<"Better luck next time!\n";
-		}
-		else{
-			cout<<"You ran out of energy and died on the island on day "<<dayCount<<endl;
-			Sleep(3000);
-			cout<<"Better luck next time!\n";
-		}
+		cout<<"You have gained: "<<endl;
 	}
-	
-	return 0;
+	cout<<weather.hunger<<" hunger,"<<endl;
+	cout<<weather.thirst<<" thirst, and"<<endl;
+	cout<<weather.energy<<" energy."<<endl;
+	hunger+=weather.hunger;
+	thirst+=weather.thirst;
+	energy+=weather.energy;
+	Sleep(3000);
 }
